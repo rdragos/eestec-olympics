8f5a11c840af2d6aa2d755403fe19a42a484469f
botezatumihaicatalin <botezatu.mihaicatalin@gmail.com>
2014-11-07 14:13:50 +0200

    First raw layout

---
+++ musicPopulator.js
@@ -0,0 +1,42 @@
+var apiKey = "d455c0755fdc3ff4c626caeefef6d9fb";
+var baseUrl = "http://ws.audioscrobbler.com/2.0"
+
+function getTopTracks() {
+
+	var method = "chart.gettoptracks";
+	var format = "json";
+	var query = $.ajax({
+		type : "GET",
+		url : baseUrl,
+		data : "api_key=" + apiKey + "&" + "method=" + method + "&" + "format=" + format
+  	});
+  	return query;
+}
+
+function populateUi() {
+
+	var request = getTopTracks();
+	request.done(function(data) {
+		console.log(data);
+		for (var index = 0; index < data.tracks.track.length; index++) {
+
+			var track = data.tracks.track[index];
+			var artistName = track.artist.name;
+			var songName = track.name;
+			var imageUrl = "http://djtt-cdn-w3cache.s3.amazonaws.com/wp-content/uploads/2011/08/online-music.bmp";
+			if ("image" in track) {
+				var imagesCount = track.image.length;
+				if (imagesCount > 0) {
+					imageUrl = track.image[imagesCount - 1]['#text'];
+				}
+			}
+
+			$("#music_list").append("<div class=\"col-xs-6 col-lg-4\"><h2>{{artistName}}</h2><p>{{songName}}</p><img src=\"{{imageUrl}}\" width = \"100\" height = \"100\"</img></div>".
+				replace("{{artistName}}",artistName).replace("{{songName}}",songName).replace("{{imageUrl}}",imageUrl));
+		}
+	});
+
+	request.fail(function (jqXHR, textStatus) {
+		alert("Cannot fetch the latest music!");
+	});
+}
